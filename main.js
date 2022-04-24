baa1b6385a0012328544facded3fb494c6a85b2c
jtogrulmammadov <togrulmemmedov082001@GMAIL.COM>
2022-04-24 22:59:41 +0400

    AddingFisrtVesion

---
+++ main.js
@@ -0,0 +1,5 @@
+// let rubl = document.querySelector("#rub");
+// console.log(rubl.getAttribute('data-value'));
+// rubl.setAttribute("data-value","AZN");
+// console.log(rubl.getAttribute('data-value'));
+  
